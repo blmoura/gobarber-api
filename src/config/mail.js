d9970ba8e4347c80bf21cb07db4db822a86c6ad2
Bruno Moura <brunomoura.blm@gmail.com>
2020-03-06 09:12:05 -0300

    Configurando Nodemailer Nessa etapa, realizamos a configuração do Nodemailer e criação da conta no mailtrap.io para testarmos o envio de e-mail no ato de cancelamento de agendamento do usuario, para que o prestador de serviço seja notificado no ato do cancelamento do agendamento.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '90c2cf4f273eee',
+    pass: '258bcb83bb437b',
+  },
+  default: {
+    from: 'Bruno Moura <noreply@brunomoura.com>',
+  },
+};
